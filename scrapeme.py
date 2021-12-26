9e0c8e9f26e60f6f8f754c4056b0a171be17a90b
tomas.macko <tomik.macko@gmail.com>
2021-12-26 20:15:30 +0100

    gitignore & unused imports

--- scrapeme.py
+++ scrapeme.py
@@ -1,18 +1,7 @@
 #!/usr/bin/env python3
 
-import click
-import sys
-
 from scripts.pet_scraper import PetScraper, get_appropriate_pets, save_data, load_json
 
-# @click.command()
-# @click.option('-s', '--sex', type=str, help='Gender of a dog')
-# @click.option('-a', '--age', type=int, help='Age of the dog. Type --help ')
-# @click.option('-h', '--height', type=int)
-# @click.option('-col1', '--color1', type=str)
-# @click.option('-col2', '--color2', type=str)
-# @click.option('-c', '--condition', type=str)
-# @click.option('f', '--fci', type=str)
 def main():
 	ps = PetScraper()
 
@@ -23,7 +12,7 @@ def main():
 	data = load_json('pets_list.json')
 	url_list = get_appropriate_pets(data)
 	save_data('pets_url.json', url_list, False)
-	print(url_list)
+
 
 if __name__ == '__main__':
 	main()
